99dd7ca38a0826518610bbad229aa0f4313c759e
Lzy0109 <752867914@qq.com>
2020-05-08 11:24:00 +0800

    完成用户端两种登录，绑定微信

--- src/main/java/com/saltedfish/community_management/config/WxConfig.java
+++ src/main/java/com/saltedfish/community_management/config/WxConfig.java
@@ -1,7 +1,7 @@
 package com.saltedfish.community_management.config;
 
 public class WxConfig {
-    //请求微信服务器需要的敏感资料（杰些都是刘哥小程序的隐私资料！）
+    //请求微信服务器需要的敏感资料（杰些都是刘哥小程序的隐私资料！！！）
     public static String AppID = "wx3300727904a36500";
     public static String AppSecret = "c24c44692b4bb4d40ee320f29a209bf0";
     public static String grant_type = "authorization_code";
